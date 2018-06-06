43b21fae40aaa0e7d28d75ef4802f0d006bf3191
rhennessey2019 <39124735+rhennessey2019@users.noreply.github.com>
2018-06-06 13:32:55 -0400

    continued with embedding project

--- htdocs/test.php
+++ htdocs/test.php
@@ -1,3 +1,21 @@
 <?php
-  header("HTTP/1.0 404 Not Found");
+
+$doc = new DOMDocument;
+
+// We don't want to bother with white spaces
+$doc->preserveWhiteSpace = false;
+
+// Most HTML Developers are chimps and produce invalid markup...
+$doc->strictErrorChecking = false;
+$doc->recover = true;
+
+$doc->loadHTMLFile('https://plusportals.com/Amsacs');
+
+$xpath = new DOMXPath($doc);
+
+$query = "/html/body/div[1]/img";
+
+$entries = $xpath->query($query);
+var_dump($entries->item(0)->textContent);
+
 ?>
